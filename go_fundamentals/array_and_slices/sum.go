132e3c3d71346f4ef5db58ffa0d62671d741618a
KaelDucatti <ducatti860@gmail.com>
2026-01-19 21:58:47 -0300

    test(sum_test): add test for void lists on SumAll()

--- go_fundamentals/array_and_slices/sum.go
+++ go_fundamentals/array_and_slices/sum.go
@@ -4,12 +4,29 @@ import "errors"
 
 func Sum(slice []int) (int, error) {
 	if len(slice) == 0 {
-		return 0, errors.New("slice must have at least 1 value")
+		return 0, errors.New("the list must have at least 1 value")
 	}
 
 	var total int
 	for _, value := range slice {
 		total += value
 	}
+	return total, nil
+}
+
+func SumAll(matrix [][]int) ([]int, error) {
+	for i := 0; i < len(matrix); i++ {
+		if len(matrix[i]) <= 0 {
+			return nil, errors.New("all lists must have at least 1 value")
+		}
+	}
+	
+	total := make([]int, len(matrix))
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			total[i] += matrix[i][j]
+		} 
+	}
+
 	return total, nil
 }
--- go_fundamentals/array_and_slices/sum_test.go
+++ go_fundamentals/array_and_slices/sum_test.go
@@ -28,7 +28,31 @@ func TestSum(t *testing.T) {
 		_, err := Sum(slice)
 
 		require.Error(err)
-		assert.EqualError(err, "slice must have at least 1 value")
+		assert.EqualError(err, "the list must have at least 1 value")
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("should return an array with the total of N slices", func (t *testing.T){
+		assert := assert.New(t)
+		require := require.New(t)
+		matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+
+		expected := []int{6, 15, 24}
+		actual, err := SumAll(matrix)
+
+		require.NoError(err)
+		assert.Equal(expected, actual)
+	})
+	t.Run("all lists must hava at least 1 value", func (t *testing.T){
+		assert := assert.New(t)
+		require := require.New(t)
+		matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+
+		_, err := SumAll(matrix)
+
+		require.Error(err)
+		assert.EqualError(err, "all lists must have at least 1 value")
 	})
 }
 
@@ -38,8 +62,20 @@ func ExampleSum() {
 	// Output: 15
 }
 
+func ExampleSumAll() {
+	total, _ := SumAll([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	fmt.Println(total)
+	// Output: []int{6, 15, 24}
+}
+
 func BenchmarkSum(b *testing.B) {
 	for b.Loop() {
 		Sum([]int{1, 2, 3, 4, 5})
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for b.Loop() {
+		SumAll([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
 	}
 }
