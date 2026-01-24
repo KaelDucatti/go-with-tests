833fc26fcd117701b879e05072a0f3d977eb0c2f
KaelDucatti <ducatti860@gmail.com>
2026-01-24 14:09:37 -0300

    refactor(perimeter_test)

--- go_fundamentals/structs_methods_interfaces/perimeter_test.go
+++ go_fundamentals/structs_methods_interfaces/perimeter_test.go
@@ -8,47 +8,51 @@ import (
 )
 
 func TestRectangle(t *testing.T) {
-	t.Run("should return the rectangle perimeter", func (t *testing.T) {
-		require := require.New(t)
-		r, err := NewRectangle(5, 4)
-		
-		expected := float32(18)
-		actual := r.Perimeter()
-
-		require.NoError(err)
-		require.Equal(expected, actual)
+	t.Run("Success Cases", func (t *testing.T) {
+		t.Run("should return the rectangle perimeter", func (t *testing.T) {
+			require := require.New(t)
+			r, err := NewRectangle(5, 4)
+			
+			expected := float32(18)
+			actual := r.Perimeter()
+	
+			require.NoError(err)
+			require.Equal(expected, actual)
+		})
+		t.Run("should return the rectangle area", func (t *testing.T) {
+			require := require.New(t)
+			r, err := NewRectangle(5, 4)
+	
+			expected := float32(20)
+			actual := r.Area()
+	
+			require.NoError(err)
+			require.Equal(expected, actual)
+		})
 	})
-	t.Run("should return the rectangle area", func (t *testing.T) {
-		require := require.New(t)
-		r, err := NewRectangle(5, 4)
-
-		expected := float32(20)
-		actual := r.Area()
-
-		require.NoError(err)
-		require.Equal(expected, actual)
-	})
-	t.Run("should return error when values are zero or negative", func (t *testing.T) {
-		require := require.New(t)
-		
-		testCases := []struct {
-			name	string
-			height	float32
-			width 	float32
-		}{
-			{"height is zero", 0, 4},
-			{"width is zero", 5, 0},
-			{"height is negative", -5, 4},
-			{"width is negative", 5, -4},
-		}
-
-		for _, tc := range testCases {
-			t.Run(tc.name, func (t *testing.T) {
-				_, err := NewRectangle(tc.height, tc.width)
-				require.Error(err)
-				require.EqualError(err, "neither value must be equal to or less than 0")
-			})
-		}
+	t.Run("Valition Errors", func (t *testing.T) {
+		t.Run("should return error when values are zero or negative", func (t *testing.T) {
+			require := require.New(t)
+			
+			testCases := []struct {
+				name	string
+				height	float32
+				width 	float32
+			}{
+				{"height is zero", 0, 4},
+				{"width is zero", 5, 0},
+				{"height is negative", -5, 4},
+				{"width is negative", 5, -4},
+			}
+	
+			for _, tc := range testCases {
+				t.Run(tc.name, func (t *testing.T) {
+					_, err := NewRectangle(tc.height, tc.width)
+					require.Error(err)
+					require.EqualError(err, "neither value must be equal to or less than 0")
+				})
+			}
+		})
 	})
 }
 
