6fc7ebe453a5c0dc93fc16634bdf9e9beddc77eb
KaelDucatti <ducatti860@gmail.com>
2026-01-24 21:26:18 -0300

    .

--- go_fundamentals/structs_methods_interfaces/shapes.go
+++ go_fundamentals/structs_methods_interfaces/shapes.go
@@ -1,11 +1,5 @@
 package structs_methods_intefaces
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-
 type Shape interface {
 	Area() 		float32
 	Perimeter() float32
@@ -22,13 +16,4 @@ type Square struct {
 
 type Circle struct {
 	Radius	float32
-}
-
-func CheckPerimeter(t *testing.T, shape Shape, expected float32) {
-	t.Helper()
-	require := require.New(t)
-
-	actual := shape.Area()
-
-	require.Equal(expected, actual)
 }
