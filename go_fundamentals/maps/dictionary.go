b713df478da5d6ef6d31c10faf298886af046773
KaelDucatti <ducatti860@gmail.com>
2026-03-21 20:26:33 -0300

    wrapping up the maps chap

--- go_fundamentals/maps/dictionary.go
+++ go_fundamentals/maps/dictionary.go
@@ -1,8 +1,10 @@
 package maps
 
 const (
-	ErrKeyNotFound      = DictionaryErr("Key Not Found in Dictionary")
-	ErrKeyAlreadyExists = DictionaryErr("Key already exists in dictionary")
+	ErrKeyNotFoundOnSearch = DictionaryErr("Key Not Found in Dictionary")
+	ErrKeyAlreadyExists    = DictionaryErr("Key already exists in dictionary")
+	ErrKeyNotFoundOnUpdate = DictionaryErr("Cannot update the key's value because it doesn't exist")
+	ErrKeyNotFoundOnDelete = DictionaryErr("Cannot delete a key that does not exist")
 )
 
 type DictionaryErr string
@@ -20,16 +22,46 @@ func NewDictionary() Dictionary {
 func (d Dictionary) Search(key string) (string, error) {
 	value, exists := d[key]
 	if !exists {
-		return "", ErrKeyNotFound
+		return "", ErrKeyNotFoundOnSearch
 	}
 	return value, nil
 }
 
 func (d Dictionary) Add(key, value string) error {
-	_, exists := d[key]
-	if exists {
+	_, err := d.Search(key)
+	switch err {
+	case ErrKeyNotFoundOnSearch:
+		d[key] = value
+	case nil:
 		return ErrKeyAlreadyExists
+	default:
+		return err
 	}
-	d[key] = value
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrKeyNotFoundOnSearch:
+		return ErrKeyNotFoundOnUpdate
+	case nil:
+		d[key] = value
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(key string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrKeyNotFoundOnSearch:
+		return ErrKeyNotFoundOnDelete
+	case nil:
+		delete(d, key)
+	default:
+		return err
+	}
 	return nil
 }
--- go_fundamentals/maps/dictionary_test.go
+++ go_fundamentals/maps/dictionary_test.go
@@ -32,31 +32,74 @@ func TestDictionarySuccessCases(t *testing.T) {
 		require.NoError(errSearch)
 		require.Equal(expected, actual)
 	})
+	t.Run("should update the key value of the dictionary", func(t *testing.T) {
+		require := require.New(t)
+		key := "test"
+		value := "this is a test"
+		dict := Dictionary{key: value}
+
+		errUpdate := dict.Update(key, "still a test")
+		expected := "still a test"
+		actual, errSearch := dict.Search(key)
+
+		require.NoError(errUpdate)
+		require.NoError(errSearch)
+		require.Equal(expected, actual)
+	})
+	t.Run("should delete the key-value of the dictionary", func(t *testing.T) {
+		require := require.New(t)
+		dict := Dictionary{"test": "this is a test"}
+
+		errDelete := dict.Delete("test")
+		_, errSearch := dict.Search("test")
+
+		require.NoError(errDelete)
+		require.Error(errSearch)
+		require.ErrorIs(errSearch, ErrKeyNotFoundOnSearch)
+	})
 }
 
 func TestDictionaryValidationErrors(t *testing.T) {
-	t.Run("should return error when key does not exists", func(t *testing.T) {
+	t.Run("should return error when key does not exist (search)", func(t *testing.T) {
 		require := require.New(t)
 		dict := Dictionary{"test": "this is a test"}
 
 		_, err := dict.Search("unknown")
 
 		require.Error(err)
-		require.ErrorIs(err, ErrKeyNotFound)
+		require.ErrorIs(err, ErrKeyNotFoundOnSearch)
 	})
-	t.Run("should return error when key already exists", func(t *testing.T) {
+	t.Run("should return error when key already exist", func(t *testing.T) {
 		require := require.New(t)	
 		key := "test"
 		value := "this is a test"
 		dict := Dictionary{key: value}
 
-		errAdd := dict.Add("test", "still a test")
+		errAdd := dict.Add(key, "still a test")
 		postValue, errSearch := dict.Search("test")
 
 		require.Error(errAdd)
 		require.NoError(errSearch)
 		require.ErrorIs(errAdd, ErrKeyAlreadyExists)
 		require.Equal(value, postValue)
+	})
+	t.Run("should return error when key does not exist (update)", func(t *testing.T) {
+		require := require.New(t)
+		dict := Dictionary{"test": "this is a test"}
+
+		err := dict.Update("unknown", "still a test")
+
+		require.Error(err)
+		require.ErrorIs(err, ErrKeyNotFoundOnUpdate)
+	})
+	t.Run("should return error when key does not exist (delete)", func(t *testing.T) {
+		require := require.New(t)
+		dict := Dictionary{"test": "this is a test"}
+
+		err := dict.Delete("unknown")
+
+		require.Error(err)
+		require.ErrorIs(err, ErrKeyNotFoundOnDelete)
 	})
 }
 
@@ -80,6 +123,20 @@ func ExampleDictionary_Search() {
 	// Output: this is a test
 }
 
+func ExampleDictionary_Update() {
+	dict := Dictionary{"test": "this is a test"}
+	dict.Update("test", "still a test")
+	fmt.Println(dict)
+	// Output: map[test:still a test]
+}
+
+func ExampleDictionary_Delete() {
+	dict := Dictionary{"test": "this is a test"}
+	dict.Delete("test")
+	fmt.Println(dict)
+	// Output: map[]
+}
+
 func BenchmarkNewDictionary(b *testing.B) {
 	for b.Loop() {
 		_ = NewDictionary()
@@ -97,5 +154,19 @@ func BenchmarkDictionary_Search(b *testing.B) {
 	dict := Dictionary{"test": "this is a test"}
 	for b.Loop() {
 		_, _ =dict.Search("test")
+	}
+}
+
+func BenchmarkDictionary_Update(b *testing.B) {
+	dict := Dictionary{"test": "this is a test"}
+	for b.Loop() {
+		dict.Update("test", "still a test")
+	}
+}
+
+func BenchmarkDictionary_Delete(b *testing.B) {
+	for b.Loop() {
+		dict := Dictionary{"test": "this is a test"}
+		dict.Delete("test")
 	}
 }
